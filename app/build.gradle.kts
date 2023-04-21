d3f7e43f5b92f80fe69a14ecb4c2f8a5ae1e0f18
Geert Bevin <gbevin@uwyn.com>
2023-04-21 16:28:23 -0400

    Updated for RIFE 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ version = 1.0
 group = "com.example"
 
 rife2 {
-    version.set("1.5.22")
+    version.set("1.6.0")
     uberMainClass.set("hello.AppSiteUber")
     useAgent.set(true)
     precompiledTemplateTypes.add(HTML)
